b560af4d8dd3b6c5ad7fa4aca4ae9b9b15f03ab9
unknown <louiecervantes@wvsu.edu.ph>
2023-04-18 14:10:35 +0800

    save changes

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -17,6 +17,23 @@ from sklearn.preprocessing import StandardScaler
 
 # Define the Streamlit app
 def app():
+    st.title('MLP Neural Network on the Titanic dataset')
+    st.subheader('by Louie F. Cervantes M.Eng., WVSU College of ICT')
+    st.write('The titanic dataset gives you information about \
+    multiple people like their ages, sexes, sibling counts, \
+    No of parent or children aboard, embarkment points and \
+    whether or not they survived the disaster. Based on these \
+    features, you have to predict if an arbitrary passenger on \
+    Titanic would survive the sinking. The question of interest \
+    for this natural dataset is how survival relates to the \
+    other attributes. There is obviously no practical need to \
+    predict survival, so the real interest is in interpretation, \
+    but success at prediction would appear to be closely related \
+    to the discovery of interesting features of the relationship. \
+    To simplify the processing, the datarows with missing values \
+    so this dataset is not the original dataset available at the \
+    machine learning websites.')
+    
     #load the dataset
     df = pd.read_csv('titanic.csv', header=0)
     st.dataframe(df, use_container_width=True)  
